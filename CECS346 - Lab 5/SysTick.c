5af7208ff311d11240cb13786590ba2427385d6b
ChaoticNerd <nat.f.kho@gmail.com>
2025-03-12 19:08:14 -0700

    Upload Lab5
    FOR REAL THIS TIME GUYS

--- CECS346 - Lab 5/SysTick.c
+++ CECS346 - Lab 5/SysTick.c
@@ -1,45 +1,59 @@
 // SysTick.c
-// Runs on TM4C123
-// Provide functions that initialize the SysTick module and 
-// function to generate a multiple of 10 milliseconds using busy wait.  
-// Original author: Daniel Valvano,Jonathan W. Valvano September 11, 2013
-// Modified by Min He on 9/24/2020, 2/22/2025.
+// Runs on LM4F120/TM4C123
+// Provide functions that initialize the SysTick module, wait at least a
+// designated number of clock cycles, and wait approximately a multiple
+// of 10 milliseconds using busy wait.  After a power-on-reset, the
+// LM4F120 gets its clock from the 16 MHz precision internal oscillator,
+// which can vary by +/- 1% at room temperature and +/- 3% across all
+// temperature ranges.  If you are using this module, you may need more
+// precise timing, so it is assumed that you are using the PLL to set
+// the system clock to 50 MHz.  This matters for the function
+// SysTick_Wait10ms(), which will wait longer than 10 ms if the clock is
+// slower.
+// Daniel Valvano
+// September 11, 2013
+// Modified by Min He on 3/7/2024
 
+/* This example accompanies the books
+   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+   Volume 1, Program 4.7
+   
+   "Embedded Systems: Real Time Interfacing to ARM Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+   Program 2.11, Section 2.6
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+#include "tm4c123gh6pm.h"
 #include <stdint.h>
 
-#define NVIC_ST_CTRL_R          (*((volatile uint32_t *)0xE000E010))
-#define NVIC_ST_RELOAD_R        (*((volatile uint32_t *)0xE000E014))
-#define NVIC_ST_CURRENT_R       (*((volatile uint32_t *)0xE000E018))
-#define NVIC_ST_CTRL_COUNT      0x00010000  // Count flag
-#define NVIC_ST_CTRL_CLK_SRC    0x00000004  // Clock Source
-#define NVIC_ST_CTRL_ENABLE     0x00000001  // Counter mode
-#define TEN_MINI_SEC 					  160000  // reload value for generating 10ms time interval for 16 MHz system clock.
+#define TEN_MS 250000  // reload value to generate 10ms for system clock 25MHz.
 
 // Initialize SysTick with busy wait running at bus clock.
 void SysTick_Init(void){
-  NVIC_ST_CTRL_R = 0;// Clear all settings
-  NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC; // setup clock source to be system clock
+  NVIC_ST_CTRL_R = 0;                   // disable SysTick during setup
+  NVIC_ST_CTRL_R |= NVIC_ST_CTRL_CLK_SRC; // set SysTick timer to use core clock: 25MHz
 }
 
 // Time delay using busy wait.
-// The delay parameter is in units of the core clock. (units of 62.5 nsec for 16 MHz clock)
-void SysTick_Wait(uint32_t delay){
-  NVIC_ST_RELOAD_R = delay - 1; // number of counts to wait  
-	NVIC_ST_CURRENT_R = 0; // any value written to CURRENT clears  
-  NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE;			// enable SysTick timer
-	while ((NVIC_ST_CTRL_R & NVIC_ST_CTRL_COUNT) == 0) { } // wait for COUNT
-  NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE;			// disable SysTick timer
-}
-
-// Time delay using busy wait.
-// This assumes 16 MHz system clock.
-// This function generate a time delay that is multiple of 10ms
-// Parameter:
-// n_ms: specify how many 10ms will be generated.
-// maximum delay can be generated: 2^32*10ms
-void SysTick_Wait10ms(uint32_t n_10ms){
-  uint32_t i;
-  for(i=0; i<n_10ms; i++){
-    SysTick_Wait(TEN_MINI_SEC);  // wait 10ms (based on 16 MHz system clock)
-  }
+// This assumes 50 MHz system clock.
+// Input: 16-bit interger for multiple of 10ms
+void SysTick_Wait10ms(uint16_t delay){	
+	NVIC_ST_RELOAD_R = TEN_MS*delay-1;
+  NVIC_ST_CURRENT_R = 0;                // any write to current clears it                                        
+  NVIC_ST_CTRL_R |= NVIC_ST_CTRL_ENABLE; // enable SysTick timer
+	
+	// wait for COUNT bit in control register to be raised.
+	while ((NVIC_ST_CTRL_R&NVIC_ST_CTRL_COUNT)==0) {} 
+  NVIC_ST_CTRL_R &= ~NVIC_ST_CTRL_ENABLE; // disable SysTick timer
 }
---
+++ CECS346 - Lab 5/SysTickPLLTest.c
@@ -0,0 +1,59 @@
+// SysTickTestMain.c
+// Runs on LM4F120/TM4C123
+// Test the SysTick functions by activating the PLL, initializing the
+// SysTick timer, and flashing an LED at a constant rate.
+// Daniel Valvano
+// September 12, 2013
+// Modified by Min He on 3/7/2024
+
+/* This example accompanies the books
+   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
+   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2013
+   Volume 1, Program 4.7
+
+   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
+   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
+   Program 2.11, Section 2.6
+
+ Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
+    You may use, edit, run or distribute this file
+    as long as the above copyright notice remains
+ THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+ OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+ MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+ VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
+ OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ For more information about my classes, my research, and my books, see
+ http://users.ece.utexas.edu/~valvano/
+ */
+
+#include "tm4c123gh6pm.h"
+#include "SysTick.h"
+#include "PLL.h"
+
+#define GREEN_LED       (*((volatile unsigned long *)0x40025010)) // double check?
+#define GREEN_LED_MASK    0x04  // bit position for onboard green LED
+
+void PORTF_Init(void);
+
+int main(void){
+	PORTF_Init();							// PF2(Blue LED) is an output for debugging
+  PLL_Init();               // set system clock to 50 MHz
+  SysTick_Init();           // initialize SysTick timer
+  while(1){
+    GREEN_LED ^= GREEN_LED_MASK; // toggle PF2: Blue LED
+    SysTick_Wait10ms(50);    // approximately 10*10 ms = 0.1s
+  }
+}
+
+void PORTF_Init(void)
+{
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R5; // activate port F
+	while ((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R5)!=SYSCTL_RCGCGPIO_R5){};
+	
+	GPIO_PORTF_DIR_R |= GREEN_LED_MASK; // make PF2 out (built-in blue LED)
+  GPIO_PORTF_AFSEL_R &= ~GREEN_LED_MASK;// disable alt funct on PF2
+  GPIO_PORTF_DEN_R |= GREEN_LED_MASK; // enable digital I/O on PF2                            
+  GPIO_PORTF_PCTL_R &= ~0x00000F00; // configure PF2 as GPIO
+  GPIO_PORTF_AMSEL_R |= GREEN_LED_MASK;   // disable analog functionality on PF  
+}
