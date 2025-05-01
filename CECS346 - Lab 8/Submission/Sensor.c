03714772ae5a3ae9b26b7ae41804dddb2aead45d
Hanna Estrada <72177607+hanna-e326@users.noreply.github.com>
2025-04-30 22:51:54 -0700

    added comments and removed magic numbers

--- CECS346 - Lab 8/Submission/Sensor.c
+++ CECS346 - Lab 8/Submission/Sensor.c
@@ -16,29 +16,29 @@
 
 // GPIO data register bit address definitions
 // Reflectance sensor pin connections:
-// PB0:ODD CTRL: right; PB1:EVEN CTRL: left
-// TODO: connect to PB and PE data
-#define LINESENSOR_CTRL       (*((volatile unsigned long *))0x40024030)
-// PE0:Sensor 0; PE1: Sensor 7 
-#define LINESENSOR						(*((volatile unsigned long *))0x4002400C)
+// PE3: Even CTRL, PE2: ODD CTRL
+// PE1: Left Sensor, PE0: Right Sensor
+#define LINESENSOR_CTRL       (*((volatile unsigned long *))0x40024030) // Port E 3,2
+#define LINESENSOR						(*((volatile unsigned long *))0x4002400C) // Port E 1,0
 	
-// This wait time should be white color pulse<WAIT_TIME<black color pulse
-#define WAIT_TIME         0 		//TODO: Fill WAIT_TIME based on Analog Measurement
+// define busy wait delays for sensor data collection
 #define TEN_MICRO_SEC			10
+#define	ONE_MILLI_SEC			1
+#define TWO_MILLI_SEC			2
+#define TEN_MILLI_SEC			10
 
 //TODO: find the right bit positions
-#define SENSOR_CTRL_PINS 0x0C   // CTRL ODD: bit 1, CRTL EVEN: bit 0
+#define SENSOR_CTRL_PINS 0x0C   // CTRL ODD: bit 2, CRTL EVEN: bit 3
 #define SENSOR_PINS      0x03   // SENSOR 0: bit 0 SENSOR 7: bit 1
 
 uint8_t Sensor_CollectData(void){
-	Sensor_Init();
-  uint8_t sensor_data;
+	Sensor_Init(); // init sensor
+  uint8_t sensor_data; // create integer sensor data
 
 	// Driving a CTRL pin low for at least 1 ms turns off the emitter LEDs
 	SENSOR_CTRL &= ~SENSOR_CTRL_PINS;
 	//Delay of 2ms
-	// nab a debounce or smthn
-	Wait_N_MS(2);
+	Wait_N_MS(TWO_MILLI_SEC);
 
 	// Turn on CTRL ODD & EVEN pins to turn on the LED emitters 
 	SENSOR_CTRL |= SENSOR_CTRL_PINS;
@@ -47,35 +47,35 @@ uint8_t Sensor_CollectData(void){
 	GPIO_PORTE_DIR_R  |= SENSOR_PINS;  // Set PE0,1 as output
 	SENSORS |= SENSOR_PINS; // Set PE0,1 HIGH
 	//Wait 10 us
-	Wait_N_US(10);
+	Wait_N_US(TEN_MICRO_SEC);
 	
 	//Set PE0,1 as inputs
 	GPIO_PORTE_DIR_R &= ~SENSOR_PINS;
 
 	//Wait 'WAIT_TIME' us
-	Wait_N_MS(1);
+	Wait_N_MS(ONE_MILLI_SEC);
 	
 	//update sensor_data with the current sensor inputs
 	sensor_data = SENSORS;	
   // turn off CTRL ODD & EVEN	pins to turn off the LED emitters to save power
 	SENSOR_CTRL &= ~SENSOR_CTRL_PINS;
 	//Wait 10ms
-	Wait_N_MS(10);
+	Wait_N_MS(TEN_MILLI_SEC);
 	return sensor_data;
 }
 
 // PORTE 1,0 connect to sensor 7/0
 // PORTE 3,2 connect to CTRL ODD/EVEN
+// Init Port E
 void Sensor_Init(void){
-	// port e init
-	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;       //Enable clock for Port B
-	while ((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R4)!=SYSCTL_RCGCGPIO_R4){}
+	SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4;       //Enable clock for Port E
+	while ((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R4)!=SYSCTL_RCGCGPIO_R4){} // Wait for clock to be ready
 
   GPIO_PORTE_DIR_R   &= ~SENSOR_PINS;  // Sensor pins are inputs
   GPIO_PORTE_DIR_R   |= ~SENSOR_CTRL_PINS; // sensor pins are outputs
-  GPIO_PORTE_AFSEL_R &= ~0x0F;  // Disable alternate function
-  GPIO_PORTE_AMSEL_R &= ~0x0F;  // Disable analog
-  GPIO_PORTE_DEN_R   |= 0x0F;   // Enable digital
+  GPIO_PORTE_AFSEL_R &= ~(SENSOR_PINS | SENSOR_CTRL_PINS);  // Disable alternate function
+  GPIO_PORTE_AMSEL_R &= ~(SENSOR_PINS | SENSOR_CTRL_PINS);  // Disable analog
+  GPIO_PORTE_DEN_R   |= (SENSOR_PINS | SENSOR_CTRL_PINS);   // Enable digital
 	
 
 }
